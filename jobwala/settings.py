2d8806c62cca2037edf6d76ca82bc003abe5c32b
yasir5405 <yasirnaseem5415@gmail.com>
2024-03-19 19:03:49 +0530

    Last try at render

--- jobwala/settings.py
+++ jobwala/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w1sr#n)@&%*7tc8z-k#+vofh%jtc8===)wo@6*6=x15f2e@($7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['10.50.0.67', '127.0.0.1']
+ALLOWED_HOSTS = ['10.50.0.67', '127.0.0.1', ]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
